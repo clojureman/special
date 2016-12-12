367aa10fc27b92bec178c08b1b6bb7e93c2a9ae3
clojureman <olsenmads@gmail.com>
2016-12-12 23:00:57 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject special "0.1.1-Beta1"
+(defproject special "0.1.2-Beta1"
+  :description "Special (Conditions). A minimalistic condition library that is not complected with exception handling. "
+  :url "https://github.com/clojureman/special"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
