408a3e48e081e938644e763bd42ec0df2b9225f3
clojureman <olsenmads@gmail.com>
2016-12-12 08:48:49 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject special "0.1.0-Beta1"
+(defproject special "0.1.1-Beta1"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
